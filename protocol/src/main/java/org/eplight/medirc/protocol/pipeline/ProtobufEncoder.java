8507f877ed57c4354d28f1d426404b4e08c7cd52
Bartosz Sławianowski <eplightning@outlook.com>
2016-04-04 15:29:39 +0200

    Automatycznie zamienianie buildera wiadomości na wiadomość

--- protocol/src/main/java/org/eplight/medirc/protocol/pipeline/ProtobufEncoder.java
+++ protocol/src/main/java/org/eplight/medirc/protocol/pipeline/ProtobufEncoder.java
@@ -1,15 +1,27 @@
 package org.eplight.medirc.protocol.pipeline;
 
 import com.google.protobuf.Message;
+import com.google.protobuf.MessageOrBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.eplight.medirc.protocol.MessageFactory;
 
-public class ProtobufEncoder extends MessageToByteEncoder<Message> {
+public class ProtobufEncoder extends MessageToByteEncoder<MessageOrBuilder> {
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Message message, ByteBuf byteBuf) throws Exception {
+    protected void encode(ChannelHandlerContext channelHandlerContext, MessageOrBuilder input, ByteBuf byteBuf) throws Exception {
+        Message message;
+
+        if (input instanceof Message) {
+            message = (Message) input;
+        } else if (input instanceof Message.Builder) {
+            message = ((Message.Builder) input).build();
+        } else {
+            // ??
+            throw new Exception("This shouldn\'t happen");
+        }
+
         short id = MessageFactory.getId(message.getClass());
 
         if (id == 0)
