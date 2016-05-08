92d1945c0fc62cbefbfa281632bd40ce8987c8f9
tezu1994 <bartosz.wysocki.94@gmail.com>
2016-05-09 00:14:01 +0200

    Klient v3: obsługa przycisków, aktualizacja list

---
+++ client/src/main/java/org/eplight/medirc/client/data/User.java
@@ -0,0 +1,19 @@
+package org.eplight.medirc.client.data;
+
+import org.eplight.medirc.protocol.Main;
+
+public class User {
+    private Main.User userMsg;
+
+    public User(Main.User userMsg) {
+        this.userMsg = userMsg;
+    }
+
+    public String getName() {
+        return userMsg.getName();
+    }
+
+    public String toString(){
+        return this.getName();
+    }
+}
--- client/src/main/java/org/eplight/medirc/client/stage/MainStage.java
+++ client/src/main/java/org/eplight/medirc/client/stage/MainStage.java
@@ -5,25 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import org.eplight.medirc.client.ClientApplication;
 import org.eplight.medirc.client.components.main.SessionCell;
 import org.eplight.medirc.client.data.Session;
+import org.eplight.medirc.client.data.User;
 import org.eplight.medirc.client.instance.network.Connection;
+import org.eplight.medirc.client.instance.network.dispatcher.JavaFxDispatchFunction;
 import org.eplight.medirc.client.instance.network.dispatcher.MessageDispatcher;
 import org.eplight.medirc.protocol.Basic;
 import org.eplight.medirc.protocol.Main;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainStage extends Stage {
 
     private Basic.HandshakeAck handshakeAck;
     private Connection connection;
     private MessageDispatcher dispatcher;
-
     @FXML
     private ListView<Session> activeSessions;
+    @FXML
+    private ListView<Session> archivedSessions;
+    @FXML
+    private ListView<User> users;
 
     public MainStage(Connection connection, Basic.HandshakeAck handshakeAck, MessageDispatcher dispatcher) {
         super();
@@ -45,20 +53,41 @@ public class MainStage extends Stage {
         setWidth(1024);
         setHeight(800);
 
-        Main.Session test = Main.Session.newBuilder().setName("Test").build();
-
-        activeSessions.getItems().add(new Session(test));
-        activeSessions.setCellFactory(param -> new SessionCell());
-
-        // WYSYŁANIE
-        /*Main.GetActiveSessionList.Builder msg = Main.GetActiveSessionList.newBuilder();
-        msg.setTest("test");
-        connection.writeAndFlush(msg.build());
+        sync();
 
         // ODBIERANIE
-        dispatcher.register(Main.ActiveSessions.class, new JavaFxDispatchFunction<>(this::asdsadads));*/
+        dispatcher.register(Main.SessionInvite.class, new JavaFxDispatchFunction<>(this::sessionInvite));
+        dispatcher.register(Main.ActiveSessions.class, new JavaFxDispatchFunction<>(this::activeSessions));
+        dispatcher.register(Main.ArchivedSessions.class, new JavaFxDispatchFunction<>(this::archivedSessions));
+        dispatcher.register(Main.UserList.class, new JavaFxDispatchFunction<>(this::userList));
+        dispatcher.register(Main.UserDisconnected.class, new JavaFxDispatchFunction<>(this::userDisconnected));
+        dispatcher.register(Main.UserConnected.class, new JavaFxDispatchFunction<>(this::userConnected));
     }
+    @FXML
+    protected void newSessionPushed() {
 
+        TextInputDialog dialog = new TextInputDialog("nowa sesja");
+        dialog.setTitle("Nowa sesja");
+        dialog.setHeaderText("Nowa sesja");
+        dialog.setContentText("Podaj nazwę sesji:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        Main.CreateNewSession.Builder msg = Main.CreateNewSession.newBuilder();
+        if (result.isPresent()) {
+            msg.setName(result.get());
+        }
+        connection.writeAndFlush(msg.build());
+    }
+    @FXML
+    protected void closePushed() {
+        this.connection.shutdown();
+        close();
+    }
+    @FXML
+    protected void disconnectPushed() {
+        this.connection.shutdown();
+    }
     public void addCloseHandlers(Runnable run) {
         setOnCloseRequest((WindowEvent ev) -> run.run());
 
@@ -69,7 +98,39 @@ public class MainStage extends Stage {
         // TODO: Z menu wyloguj
     }
 
-    public void asdsadads(Main.ActiveSessions msg) {
-
+    public void sync(){
+        Main.SyncRequest.Builder msg = Main.SyncRequest.newBuilder();
+        connection.writeAndFlush(msg.build());
+    }
+    public void activeSessions(Main.ActiveSessions msg){
+        activeSessions.getItems().clear();
+        for(Main.Session s : msg.getSessionList()) {
+            activeSessions.getItems().add(new Session(s));
+            activeSessions.setCellFactory(param -> new SessionCell());
+        }
+    }
+    public void archivedSessions(Main.ArchivedSessions msg){
+        archivedSessions.getItems().clear();
+        for(Main.Session s : msg.getSessionList()) {
+            archivedSessions.getItems().add(new Session(s));
+            archivedSessions.setCellFactory(param -> new SessionCell());
+        }
+    }
+    public void userList(Main.UserList msg){
+        users.getItems().clear();
+        if(!msg.getUserList().isEmpty()) {
+            for (Main.User u : msg.getUserList()) {
+                users.getItems().add(new User(u));
+            }
+        }
+    }
+    public void userConnected(Main.UserConnected msg){
+        sync();
+    }
+    public void userDisconnected(Main.UserDisconnected msg){
+        sync();
+    }
+    public void sessionInvite(Main.SessionInvite msg) {
+        sync();
     }
 }
